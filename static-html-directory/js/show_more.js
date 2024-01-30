812f9cbe2d7374caaf88c14a399204154cdcf8c4
mgraziani <60097906+mgraziani@users.noreply.github.com>
2024-01-30 19:02:06 +0100

    added obtained results

--- static-html-directory/js/show_more.js
+++ static-html-directory/js/show_more.js
@@ -1,18 +1,15 @@
-function showmoreFunction(more_name,btn_name,img_name) {
+function showmoreFunction(more_name,btn_name) {
     
     var moreText = document.getElementById(more_name);
     var btnText = document.getElementById(btn_name);
-    var imgastro = document.getElementById(img_name);
     
     if (moreText.style.display === "inline") {
 	btnText.innerHTML = "Leggi di più";
 	moreText.style.display = "none";
-	imgastro.style.display = "inline";
     }
     else {
 	btnText.innerHTML = "Chiudi";
 	btnText.style.marginBottom = "100px";
 	moreText.style.display = "inline";
-	imgastro.style.display = "none";
     }
 }
