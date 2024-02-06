14063a587bb3cb054207a9c0a7e59a1c5ea8c016
mgraziani <60097906+mgraziani@users.noreply.github.com>
2024-02-06 17:20:48 +0100

    added events

---
+++ static-html-directory/js/change_menu.js
@@ -0,0 +1,17 @@
+function change_menu_2023() {
+    document.getElementById("date2023").style.background = "#041725";
+    document.getElementById("btn_date2023").style.color = "white";
+    document.getElementById("date2024").style.background = "white";
+    document.getElementById("btn_date2024").style.color = "#041725";
+   document.getElementById("riga2023").style.display = "contents";
+   document.getElementById("riga2024").style.display = "none";
+}
+
+function change_menu_2024() {
+    document.getElementById("date2024").style.background = "#041725";
+    document.getElementById("btn_date2024").style.color = "white";
+    document.getElementById("date2023").style.background = "white";
+    document.getElementById("btn_date2023").style.color = "#041725";
+   document.getElementById("riga2024").style.display = "contents";
+    document.getElementById("riga2023").style.display = "none";
+}
