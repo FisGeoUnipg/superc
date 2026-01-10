97a863eb5f23c918a95b4e4203c3aff45cb57485
mgraziani <60097906+mgraziani@users.noreply.github.com>
2026-01-10 14:54:40 +0100

    fix velocità caricamento

---
+++ static-html-directory/risultati_progetti/pagine_da_integrare/img_camera_bianca/convert.sh
@@ -0,0 +1,7 @@
+avifenc -q 60 figura1.jpg figura1.avif
+avifenc -q 60 figura2.jpg figura2.avif
+avifenc -q 60 figura3.png figura3.avif
+avifenc -q 60 figura4.jpg figura4.avif
+avifenc -q 60 figura5.jpg figura5.avif
+avifenc -q 60 figura6.jpg figura6.avif
+avifenc -q 60 figura7.jpg figura7.avif
---
+++ static-html-directory/risultati_progetti/pagine_da_integrare/img_news/convert.sh
@@ -0,0 +1,3 @@
+avifenc -q 60 et_telescope_banner.png et_telescope_banner.avif
+avifenc -q 60 et_telescope.jpg et_telescope.avif
+avifenc -q 60 physics_game_accademy.png physics_game_accademy.avif
